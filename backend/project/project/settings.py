8094fc7c1b49b310d7b767643873ccba3bedc63e
arbutnar <butnaruariannaa@gmail.com>
2024-06-26 15:37:56 +0200

    tmp

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -16,20 +16,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z1whsw@(6gfnu%)lsrv%(m^^21enww2h*@bk%a(rrpmz3!wha9'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',

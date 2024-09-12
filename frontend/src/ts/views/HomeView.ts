06b69114b0990f70de085c8f52b291e0f90475c2
arbutnar <butnaruariannaa@gmail.com>
2024-09-12 10:13:51 +0200

    test

--- frontend/src/ts/views/HomeView.ts
+++ frontend/src/ts/views/HomeView.ts
@@ -128,7 +128,7 @@ export default class extends AView {
 									<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
 								</svg>
 							</div>
-							<div class="grow h-full bg-wf-primary rounded-lg"></div>
+							<div class="grow h-full bg-black rounded-lg mt-3"></div>
 							<div class="px-3">
 								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
 									<path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
